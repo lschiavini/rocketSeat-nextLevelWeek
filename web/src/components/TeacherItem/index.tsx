e96a1ec2c153e5b6fe92dcd5d730f3839e471a32
Lucas Skira <lucas.schiavini@coreconsulting.com.br>
2020-08-08 18:20:54 -0300

    creates new connection when clicking whatsappIcon

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.css';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api';
 
 export interface Teacher {
     id: number;
@@ -18,6 +19,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection () {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
     return (
         <article className="teacher-item">
                 <header>
@@ -37,7 +44,11 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
                         Preço/hora
                         <strong>R$ {teacher.cost}</strong>
                     </p>
-                    <a href={`https://wa.me/${teacher.whatsapp}`}>
+                    <a 
+                        target="_blank"
+                        onClick={createNewConnection} 
+                        href={`https://wa.me/${teacher.whatsapp}`}
+                    >
                         <img src={whatsappIcon} alt="WhatsApp"/>
                         Entrar em contato
                     </a>
