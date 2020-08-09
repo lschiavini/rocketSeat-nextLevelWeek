79d5e0705ed046aaafbd1394f7212e4aceca1a81
Lucas Skira <lucas.schiavini@coreconsulting.com.br>
2020-08-09 11:00:26 -0300

    color icons

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -45,9 +45,9 @@ function StudyTabs() {
             <Screen 
                 options={{
                     tabBarLabel: 'Proffys',
-                    tabBarIcon: ({ color, size }) => {
+                    tabBarIcon: ({ color, size, focused }) => {
                         return(
-                            <Ionicons name="ios-easel" size={size} color={color} />
+                            <Ionicons name="ios-easel" size={size} color={focused? '#8257e5' : color} />
                         );
                     }
                 }}
@@ -55,9 +55,9 @@ function StudyTabs() {
             <Screen 
                 options={{
                     tabBarLabel: 'Favoritos',
-                    tabBarIcon: ({ color, size }) => {
+                    tabBarIcon: ({ color, size, focused }) => {
                         return(
-                            <Ionicons name="ios-heart" size={size} color={color} />
+                            <Ionicons name="ios-heart" size={size} color={focused? '#8257e5' : color} />
                         );
                     }
                 }}
